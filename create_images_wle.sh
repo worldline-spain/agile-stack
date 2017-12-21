e2bc84360a2361920f5f320cc2a5a74340b04159
Jordi Escudero <jordi.escudero@worldline.com>
2017-12-21 16:22:26 +0100

    iii

--- create_images_wle.sh
+++ create_images_wle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker build -f wle/Dockerfile_wle-agile-core --tag="jordiescudero/wle-agile-core:1.2.5" wle
 docker build -f wle/Dockerfile_wle-agile-zb-protocol --tag="jordiescudero/wle-agile-zb-protocol:1.2.5" wle 
-docker build -f wle/Dockerfile_wle-agile-dbus --tag="jordiescudero/wle-agile-dbus:1.2.5" wle
+docker build -f wle/Dockerfile_wle-agile-dbus --tag="jordiescudero/wle-agile-dbus:1.3" wle
 docker build -f wle/Dockerfile_wle-agile-ui --tag="jordiescudero/wle-agile-ui:1.2.5" wle
 docker build -f wle/Dockerfile_wle-agile-osjs --tag="jordiescudero/wle-agile-osjs:1.2.5" wle
 docker build -f wle/Dockerfile_wle-agile-nodered --tag="jordiescudero/wle-agile-nodered:1.2.5" wle
@@ -9,7 +9,7 @@ docker build -f wle/Dockerfile_wle-agile-security --tag="jordiescudero/wle-agile
 
 docker push jordiescudero/wle-agile-core:1.2.5
 docker push jordiescudero/wle-agile-zb-protocol:1.2.5
-docker push jordiescudero/wle-agile-dbus:1.2.5
+docker push jordiescudero/wle-agile-dbus:1.3
 docker push jordiescudero/wle-agile-ui:1.2.5
 docker push jordiescudero/wle-agile-osjs:1.2.5
 docker push jordiescudero/wle-agile-nodered:1.2.5
